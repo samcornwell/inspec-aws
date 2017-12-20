3cac4326422a1b9ec7c6ca19ff0663392cb66ca3
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2017-12-20 16:02:59 -0500

    modified aws_s3_bucket_object control to use the s3_bucket_name attribute rather than hard-coded name
    Signed-off-by: Sam Cornwell <14048146+samcornwell@users.noreply.github.com>

--- test/integration/default/verify/controls/aws_s3_bucket_object.rb
+++ test/integration/default/verify/controls/aws_s3_bucket_object.rb
@@ -1,5 +1,15 @@
+fixtures = {}
+[
+  's3_bucket_name',
+].each do |fixture_name|
+  fixtures[fixture_name] = attribute(
+    fixture_name,
+    default: "default.#{fixture_name}",
+    description: 'See ../build/s3.tf',
+  )
+end
 
-describe aws_s3_bucket_object(name: '-aws_demo_s3_bucket', key: 'public-pic-authenticated.jpg') do
+describe aws_s3_bucket_object(name: fixtures['s3_bucket_name'], key: 'public-pic-authenticated.jpg') do
   it { should exist }
   it { should be_public }
   its('auth_users_permissions') { should cmp [] }
