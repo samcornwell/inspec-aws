85319890ec904259377683d0d618dbbf3a37ca4f
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-02-07 22:43:42 -0500

    incomplete untested new aws_s3_bucket_object resource
    Signed-off-by: Sam Cornwell <14048146+samcornwell@users.noreply.github.com>

--- libraries/aws_s3_bucket_object.rb
+++ libraries/aws_s3_bucket_object.rb
@@ -1,48 +1,36 @@
 require '_aws'
 
-# author: Matthew Dromazos
-class AwsS3Bucket < Inspec.resource(1)
-  name 'aws_s3_bucket'
-  desc 'Verifies settings for a s3 bucket'
+class AwsS3BucketObject < Inspec.resource(1)
+  name 'aws_s3_bucket_object'
+  desc 'Verifies settings for a s3 bucket objects'
   example "
-    describe aws_s3_bucket(bucket_name: 'test_bucket') do
+    describe aws_s3_bucket_object(bucket_name: 'test_bucket', object_key 'test_object') do
       it { should exist }
     end
   "
 
   include AwsResourceMixin
-  attr_reader :bucket_name, :region
+  attr_reader :bucket_name, :object_key, :region
 
   def to_s
-    "S3 Bucket #{@bucket_name}"
+    "S3 Bucket Object #{@bucket_name}/#{@object_key}"
   end
 
-  def bucket_acl
+  def object_acl
     # This is simple enough to inline it.
-    @bucket_acl ||= AwsS3Bucket::BackendFactory.create.get_bucket_acl(bucket: bucket_name).grants
+    @object_acl ||= AwsS3BucketObject::BackendFactory.create.get_object_acl(bucket: bucket_name, key: object_key).grants
   end
 
   def bucket_policy
     @bucket_policy ||= fetch_bucket_policy
   end
 
-  def bucket_objects
-    @bucket_objects ||= AwsS3Bucket::BackendFactory.create.list_objects_v2(bucket: bucket_name).contents
-  end
-
-  # RSpec will alias this to be_public
   def public?
     # first line just for formatting
     false || \
-      bucket_acl.any? { |g| g.grantee.type == 'Group' && g.grantee.uri =~ /AllUsers/ } || \
-      bucket_acl.any? { |g| g.grantee.type == 'Group' && g.grantee.uri =~ /AuthenticatedUsers/ } || \
+      object_acl.any? { |g| g.grantee.type == 'Group' && g.grantee.uri =~ /AllUsers/ } || \
+      object_acl.any? { |g| g.grantee.type == 'Group' && g.grantee.uri =~ /AuthenticatedUsers/ } || \
       bucket_policy.any? { |s| s.effect == 'Allow' && s.principal == '*' }
-  end
-
-  def has_access_logging_enabled?
-    return unless @exists
-    # This is simple enough to inline it.
-    !AwsS3Bucket::BackendFactory.create.get_bucket_logging(bucket: bucket_name).logging_enabled.nil?
   end
 
   private
@@ -50,19 +38,18 @@ class AwsS3Bucket < Inspec.resource(1)
   def validate_params(raw_params)
     validated_params = check_resource_param_names(
       raw_params: raw_params,
-      allowed_params: [:bucket_name],
-      allowed_scalar_name: :bucket_name,
-      allowed_scalar_type: String,
+      allowed_params: [:bucket_name, :object_key]
     )
-    if validated_params.empty? or !validated_params.key?(:bucket_name)
-      raise ArgumentError, 'You must provide a bucket_name to aws_s3_bucket.'
+
+    if validated_params.empty? or !validated_params.key?(:bucket_name) or !validated_params.key?(:object_key)
+      raise ArgumentError, 'You must provide a bucket_name and object_key to aws_s3_bucket_object.'
     end
 
     validated_params
   end
 
   def fetch_from_aws
-    backend = AwsS3Bucket::BackendFactory.create
+    backend = AwsS3BucketObject::BackendFactory.create
 
     # Since there is no basic "get_bucket" API call, use the
     # region fetch as the existence check.
@@ -72,11 +59,12 @@ class AwsS3Bucket < Inspec.resource(1)
       @exists = false
       return
     end
+    # TODO: actually lookup object
     @exists = true
   end
 
   def fetch_bucket_policy
-    backend = AwsS3Bucket::BackendFactory.create
+    backend = AwsS3BucketObject::BackendFactory.create
 
     begin
       # AWS SDK returns a StringIO, we have to read()
@@ -96,7 +84,7 @@ class AwsS3Bucket < Inspec.resource(1)
     class AwsClientApi
       BackendFactory.set_default_backend(self)
 
-      def get_bucket_acl(query)
+      def get_object_acl(query)
         AWSConnection.new.s3_client.get_bucket_acl(query)
       end
 
@@ -106,14 +94,6 @@ class AwsS3Bucket < Inspec.resource(1)
 
       def get_bucket_policy(query)
         AWSConnection.new.s3_client.get_bucket_policy(query)
-      end
-
-      def get_bucket_logging(query)
-        AWSConnection.new.s3_client.get_bucket_logging(query)
-      end
-
-      def list_objects_v2(query)
-        AWSConnection.new.s3_client.list_objects_v2(query)
       end
     end
   end
